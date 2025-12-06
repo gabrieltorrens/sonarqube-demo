d717d1e96337f31e69fe1f91da56174f29229e48
gabrieltorrens <50712426+gabrieltorrens@users.noreply.github.com>
2025-12-06 18:11:01 -0500

    Fix casing in return message for correct number
    Corrected the casing of 'Number' in the return statement.

--- install.py
+++ install.py
@@ -4,7 +4,7 @@ def main():
     if number <> 4:
         break #should not be used outside a loop
     else:
-        return("Correct number")
+        return("Correct Number")
 
 if __name__ == "__main__":
     print(main())
